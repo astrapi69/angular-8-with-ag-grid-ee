81c55ff74d9c1ccc6d86613b95032fea37735459
Asterios Raptis <net.asterios.raptis@mhp.com>
2020-04-23 16:21:28 +0200

    an example for ag-grid

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,88 @@
+import {Component, OnInit} from '@angular/core';
+
+import {GridApi, GridOptions} from "@ag-grid-community/all-modules";
+import {Module} from "@ag-grid-community/core";
+import {AllModules} from "@ag-grid-enterprise/all-modules";
+
+@Component({
+    selector: 'app-dashboard',
+    templateUrl: './dashboard.component.html',
+    styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+    gridOptions: GridOptions;
+    gridApi: GridApi;
+    columnDefs;
+    defaultColDef;
+    frameworkComponents;
+    multiSortKey;
+    context;
+    modules: Module[] = AllModules;
+    rowData: any[] = [];
+
+    constructor() {
+    }
+
+    ngOnInit() {
+
+        this.multiSortKey = "ctrl";
+        this.gridOptions = {
+            rowHeight: 40,
+            floatingFilter: true,
+        };
+        this.columnDefs = [
+            {field: 'make'},
+            {field: 'model'},
+            {field: 'price'}
+        ];
+        this.defaultColDef = {
+            editable: false,
+            sortable: true,
+            minWidth: 100,
+            filter: true,
+        };
+        this.rowData = [
+            {make: 'Toyota', model: 'Celica', price: 35000},
+            {make: 'Ford', model: 'Mondeo', price: 32000},
+            {make: 'Porsche', model: 'Boxter', price: 72000}
+        ];
+    }
+
+    onGridReady(params) {
+        this.gridApi = params.api;
+        this.gridOptions.api = this.gridApi;
+        this.gridApi.sizeColumnsToFit();
+        // this can be changed with a subscribe from a service
+        params.api.setRowData(this.rowData);
+
+    }
+
+    public onFirstDataRendered(params) {
+        params.api.sizeColumnsToFit();
+    }
+
+
+    public onGridSizeChanged(params) {
+        const gridWidth = document.getElementById("grid-wrapper").offsetWidth;
+        const columnsToShow = [];
+        const columnsToHide = [];
+        let totalColsWidth = 0;
+        const allColumns = params.columnApi.getAllColumns();
+        if (allColumns.length) {
+            for (let i = 0; i < allColumns.length; i++) {
+                const column = allColumns[i];
+                totalColsWidth += column.getMinWidth();
+                if (totalColsWidth > gridWidth) {
+                    columnsToHide.push(column.colId);
+                } else {
+                    columnsToShow.push(column.colId);
+                }
+            }
+            params.columnApi.setColumnsVisible(columnsToShow, true);
+            params.columnApi.setColumnsVisible(columnsToHide, false);
+            params.api.sizeColumnsToFit();
+        }
+    }
+
+}
